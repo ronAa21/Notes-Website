fa4af1ce171b6471b42a732e9f09ad37b701502d
ronAa21 <aaronpagente19@gmail.com>
2026-01-16 12:08:21 +0800

    Added error states and modal

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import notes from "./routes/notes.js";
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 10000;
+
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -12,6 +14,6 @@ app.use("/api", notes);
 
 app.use((req, res) => res.status(404).send("404 not found"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running at ${process.env.PORT}\nOkairi ${process.env.USER}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at ${PORT}\nOkairi ${process.env.USER}`);
 })
